7e97ce9070beb1ee6b363fa7dfac218da397ddfd
Jenni Ristonmaa <jenni.ristonmaa@opuscapita.com>
2018-07-26 15:26:13 +0300

    repo template

---
+++ src_docs/components/example.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Example from '../../src/index';
+
+export default class ComponentView extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div style={{ padding: '20px' }}>
+        <Example />
+      </div>
+    );
+  }
+}
